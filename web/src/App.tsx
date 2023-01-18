510de9f3188132f162dfb00540294b2ea100e754
lucasnwvalladares <lucasnwvalladares@gmail.com>
2023-01-18 10:36:30 -0300

    day 2 - web update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,15 @@
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
-import { Habit } from './components/Habit'
+// import { Habit } from './components/Habit'
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={23} />
-      <Habit completed={5} />
-      <Habit completed={15} />
-      <Habit completed={63} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
