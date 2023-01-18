5ea33ccaaaae653232d16d1874a20f938316edc9
lucasnwvalladares <lucasnwvalladares@gmail.com>
2023-01-17 22:43:33 -0300

    day 2 - back-end update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,11 @@
 import Fastify from "fastify"
 import fastifyCors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(fastifyCors)
-
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Fazer'
-      }
-    }
-  })
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333,
