e4733103e1c54e054efcd4c02851ff6167d9c8a9
lucasnwvalladares <lucasnwvalladares@gmail.com>
2023-01-20 19:12:07 -0300

    day 4 - mobile update

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
-import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado']
 
 export function NewHabit() {
   const [weekDays, setWeekDays] = useState<number[]>([])
-  
+  const [title, setTitle] = useState('')
+
   function handleToggleWeekDays(weekDayIndex: number) {
     if (weekDays.includes(weekDayIndex)) {
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
@@ -19,6 +21,24 @@ export function NewHabit() {
       setWeekDays(prevState => [...prevState, weekDayIndex])
     }
   }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha os dias.')
+      }
+
+      await api.post('/habits', { title, weekDays })
+      setTitle('')
+      setWeekDays([])
+
+      Alert.alert('Novo Hábito', 'Hábito criado com sucesso!')
+    } catch (error) {
+      Alert.alert('Ops!!!', 'Não foi possível criar o novo hábito.')
+      throw error
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView 
@@ -41,6 +61,8 @@ export function NewHabit() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="ex.: 2h de Exercícios Físicos"
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text
@@ -63,6 +85,7 @@ export function NewHabit() {
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather
             name="check"
--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -26,7 +26,7 @@ export function HabitDay({completed = 0, amount = 0, date}: HabitDayProps) {
           'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
           'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
           'bg-violet-600 border-violet-400': completedPercentage >= 60 && completedPercentage < 80,
-          'bg-violet-500 border-violet-300': completedPercentage >= 80,
+          'bg-violet-500 border-violet-300': completedPercentage >= 80
         })}
       />
 
